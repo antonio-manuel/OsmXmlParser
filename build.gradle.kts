791c4c4357378a1c0da87165c5af315cdb0b5be8
Antonio Lopez <antonio.lopez@n26.com>
2023-05-10 14:50:01 +0200

    Updated gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+object Versions {
+    const val JACKSON_KOTLIN = "2.15.0"
+}
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("org.ajoberstar.grgit") version "5.2.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
+}
+
+group = "eu.antoniolopez.xmlparser"
+version = grgit.head().abbreviatedId.take(6)
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation(group = "com.liferay", name = "com.fasterxml.jackson.databind", version = "2.10.5.1.LIFERAY-PATCHED-1")
+    implementation(group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-xml", version= Versions.JACKSON_KOTLIN)
+    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version= Versions.JACKSON_KOTLIN)
+    implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version= Versions.JACKSON_KOTLIN)
+    implementation(group = "com.fasterxml.jackson.core", name = "jackson-annotations", version= Versions.JACKSON_KOTLIN)
+    implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version= Versions.JACKSON_KOTLIN)
+}
